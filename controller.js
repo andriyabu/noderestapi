ae393c758ddfb700b4974e1b27b03989d6f49288
Andri Yabu <andriyabu@gmail.com>
2020-12-18 10:41:08 +0700

    show all users data

--- controller.js
+++ controller.js
@@ -6,3 +6,13 @@ let connection = require('./connection');
 exports.index = (req,res) => {
     response.ok("REST API Application running seccessfully",res);
 };
+
+exports.allusers = (req,res) => {
+    connection.query('SELECT * FROM users',(error,rows,fileds) => {
+        if (error) {
+            connection.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    });
+}
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = (app) => {
     let mycontroller = require('./controller');
 
     app.route('/').get(mycontroller.index);
+    app.route('/users').get(mycontroller.allusers);
 }
+
