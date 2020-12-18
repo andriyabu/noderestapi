02b97bd883a5ee09f8e0b14187dd8bfd4a97eb03
Andri Yabu <andriyabu@gmail.com>
2020-12-18 11:22:05 +0700

    create store and update function on userscontroller then create routes

--- routes.js
+++ routes.js
@@ -7,5 +7,7 @@ module.exports = (app) => {
     app.route('/').get(homeController.index);
     app.route('/users').get(usersController.index);
     app.route('/users/:id').get(usersController.show);
+    app.route('/users').post(usersController.store);
+    app.route('/users/:id').put(usersController.update);
 }
 
--- userscontroller.js
+++ userscontroller.js
@@ -13,10 +13,46 @@ exports.index = (req,res) => {
     });
 }
 
+// show users data by id
 exports.show = (req,res) => {
     let id = req.params.id
     connection.query('SELECT * FROM users WHERE id = ?',[id], (error,rows,fields) => {
         if (error){
+            console.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    });
+}
+
+// insert data to users table
+exports.store = (req,res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+    let address = req.body.address;
+
+    connection.query('INSERT INTO users(name,email,phone,address) VALUES(?,?,?,?)',
+    [name,email,phone,address], (error,rows,fields) => {
+        if (error) {
+            console.log(error);
+        }else{
+            response.ok(rows,res);
+        }
+    });
+}
+
+//update users data by id
+exports.update = (req,res) => {
+    let id = req.params.id
+    let name = req.body.name;
+    let email = req.body.email;
+    let phone = req.body.phone;
+    let address = req.body.address;
+
+    connection.query('UPDATE users SET name=? , email=?,phone=?,address=? WHERE id=?',
+    [name,email,phone,address,id], (error,rows,fields) => {
+        if (error) {
             console.log(error);
         }else{
             response.ok(rows,res);
